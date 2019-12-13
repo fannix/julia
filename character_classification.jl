55bc1281498bd7311e1eb0eb47179f58c8d684ed
xmeng <mxf3306@gmail.com>
2019-12-13 23:21:17 +0000

    use convolution neural network

--- character_classification.jl
+++ character_classification.jl
@@ -25,10 +25,51 @@ arr = [Float64.(e) for e in img_arr]
 using Flux
 using Flux: onehotbatch
 
-labelbatch = onehotbatch(labelset, string.(collect('A':'Z')))
-
 using Images
 img_size = (40, 40)
 resize_img = imresize.(arr, img_size...)
 
-img_batch = cat(resize_img..., dims=3)
+# Data should be stored in WHCN order (width, height, # channels, # batches). 
+# https://fluxml.ai/Flux.jl/v0.10/models/layers/#Flux.Dense
+
+set_len = length(pathset)
+
+import Base.Iterators.partition
+
+# Bundle images together with labels and group into minibatchess
+function make_minibatch(X, Y, idxs)
+    X_batch = Array{Float32}(undef, size(X[1])..., 1, length(idxs))
+    for i in 1:length(idxs)
+        X_batch[:, :, :, i] = Float32.(X[idxs[i]])
+    end
+    Y_batch = onehotbatch(Y[idxs], string.(collect('A':'Z')))
+    return (X_batch, Y_batch)
+end
+
+batch_size = 10
+
+train = [make_minibatch(resize_img, labelset, idx) for idx in  partition(1:set_len, batch_size)]
+
+
+model = Chain(
+    Conv((3, 3), 1 => 16, pad=(1, 1), relu),
+    MaxPool((2, 2)),
+    Conv((3, 3), 16 => 32, pad=(1, 1), relu),
+    MaxPool((2, 2)),
+    x -> reshape(x, :, size(x, 4)),
+    Dense(10*10*32, 26),
+    softmax
+)
+
+model(train[1][1])
+
+loss(x, y) = crossentropy(model(x), y)
+
+
+evalcb = () -> @show(loss(train[1][1], train[1][2]))
+
+using Flux: @epochs
+
+@epochs 10 Flux.train!(loss, params(model), train, ADAM(), cb = throttle(evalcb, 10))
+
+# accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
