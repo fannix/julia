8688f41ae0849fbc05a8588e62ea8347aeac3202
xmeng <mxf3306@gmail.com>
2019-12-19 23:31:35 +0000

    refactoring

--- character_classification.jl
+++ character_classification.jl
@@ -1,41 +1,13 @@
-
 using FileIO
-
-folder = joinpath("/", "home", "xmeng", "projects", "julia")
-
-trainset = []
-
-for label in 'A':'Z'
-    path = joinpath(folder, string(label))
-    if isdir(path)
-        files = readdir(path)
-        for file in files
-            push!(trainset, (joinpath(path, file), label))
-        end
-    end
-end
-
-pathset = [path for (path, _) in trainset]
-labelset = [label for (_, label) in trainset]
-
-img_arr = load.(pathset)
-arr = [Float64.(e) for e in img_arr]
-
 using Flux
 using Flux: onehotbatch
-
+using Flux: @epochs, throttle, crossentropy, onecold
 using Images
-img_size = (40, 40)
-resize_img = imresize.(arr, img_size...)
-
-# Data should be stored in WHCN order (width, height, # channels, # batches). 
-# https://fluxml.ai/Flux.jl/v0.10/models/layers/#Flux.Dense
-
-set_len = length(pathset)
-
 import Base.Iterators.partition
+using Statistics: mean
 
-# Bundle images together with labels and group into minibatchess
+batch_size = 10
+
 function make_minibatch(X, Y, idxs)
     X_batch = Array{Float32}(undef, size(X[1])..., 1, length(idxs))
     for i in 1:length(idxs)
@@ -45,10 +17,42 @@ function make_minibatch(X, Y, idxs)
     return (X_batch, Y_batch)
 end
 
-batch_size = 10
+
+function readdata(folder)
+    dataset = []
+    for label in 'A':'Z'
+        path = joinpath(folder, string(label))
+        if isdir(path)
+            files = readdir(path)
+            for file in files
+                push!(dataset, (joinpath(path, file), label))
+            end
+        end
+    end
+
+    pathset = [path for (path, _) in dataset]
+    labelset = [label for (_, label) in dataset]
+
+    img_arr = load.(pathset)
+    arr = [Float64.(e) for e in img_arr]
+
+    img_size = (40, 40)
+    resize_img = imresize.(arr, img_size...)
+
+    return resize_img, labelset
+end
+
+trainfolder = joinpath("/", "home", "xmeng", "projects", "julia", "train")
+resize_img, labelset = readdata(trainfolder)
+
+set_len = length(labelset)
 
 train = [make_minibatch(resize_img, labelset, idx) for idx in  partition(1:set_len, batch_size)]
 
+# Data should be stored in WHCN order (width, height, # channels, # batches). 
+# https://fluxml.ai/Flux.jl/v0.10/models/layers/#Flux.Dense
+
+# Bundle images together with labels and group into minibatchess
 
 model = Chain(
     Conv((3, 3), 1 => 16, pad=(1, 1), relu),
@@ -60,26 +64,18 @@ model = Chain(
     softmax
 )
 
-model(train[1][1])
+# model(train[1][1])
 
-using Flux: @epochs, throttle, crossentropy, onecold
-
+accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
 loss(x, y) = crossentropy(model(x), y)
-
 
 evalcb = () -> @show(loss(train[1][1], train[1][2]))
 
-
 @epochs 50 Flux.train!(loss, params(model), train, ADAM(), cb = throttle(evalcb, 10))
 
-using Statistics: mean
+# testX = cat(resize_img..., dims=4)
 
-accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
-
-testX = cat(resize_img..., dims=4)
-
-
-@show accuracy(testX, onehotbatch(labelset, Vector('A':'Z')))
+# @show accuracy(testX, onehotbatch(labelset, Vector('A':'Z')))
 
 # predict_y = onecold(model(testX))
 # actual_y = labelset
@@ -88,17 +84,22 @@ testX = cat(resize_img..., dims=4)
 # df[df.actual .!= df.predict , :]
 
 # sklearn
-using ScikitLearn
+# using ScikitLearn
 
 # This model requires scikit-learn. See
 # http://scikitlearnjl.readthedocs.io/en/latest/models/#installation
-@sk_import linear_model: LogisticRegression
+# @sk_import linear_model: LogisticRegression
 
-X = hcat(reshape.(resize_img, :)...)'
-y = string.(labelset)
+# X = hcat(reshape.(resize_img, :)...)'
+# y = string.(labelset)
 
-model = LogisticRegression(fit_intercept=true)
-fit!(model, X, y)
+# model = LogisticRegression(fit_intercept=true)
+# fit!(model, X, y)
 
-accuracy = sum(predict(model, X) .== y) / length(y)
-println("accuracy: $accuracy")
+# accuracy = sum(predict(model, X) .== y) / length(y)
+# println("accuracy: $accuracy")
+
+testfolder = joinpath("/", "home", "xmeng", "projects", "julia", "test")
+testset, label = readdata(testfolder)
+test = cat(testset..., dims=4)
+@show accuracy(test, onehotbatch(label, Vector('A':'Z')))
